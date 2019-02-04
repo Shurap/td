dc074c11eba49cf9ba693c4d198849269aca1261
Shurap <bap.essp@mail.ru>
2019-02-04 21:40:09 +0300

    first branch commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import styles from './Header.module.css';
 
-const Header = () => (
+import {connect} from "react-redux";
+
+const Header = (props) => (
   <div className={styles.header}>
+    <h2>training diary</h2>
     <Link to='/signup'>Sign Up</Link>
     <Link to='/signin'>Sign In</Link>
     <Link to='/signout'>Sign Out</Link>
     <Link to='/home'>Home</Link>
+    <p>{props.userName}</p>
   </div>
 );
 
-export default Header;
+const mapStateToProps = (state) => ({userName : state.main.userName});
+
+export default connect(mapStateToProps)(Header);
