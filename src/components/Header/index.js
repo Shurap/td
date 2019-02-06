270d5b454c292a932e01e683b8917cdbba87b10c
Shurap <bap.essp@mail.ru>
2019-02-06 21:52:03 +0300

    finished css header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 
 const Header = (props) => (
-  <div className={styles.header}>
-    <h2>training diary</h2>
-    <Link to='/signup'>Sign Up</Link>
-    <Link to='/signin'>Sign In</Link>
-    <Link to='/signout'>Sign Out</Link>
-    <Link to='/home'>Home</Link>
-    <p>{props.userName}</p>
-  </div>
+  <header className={styles.header}>
+    <h1>training diary</h1>
+    <nav>
+      <Link to='/signup'>Sign Up</Link>
+      <Link to='/signin'>Sign In</Link>
+      <Link to='/home'>Home</Link>
+    </nav>
+    <div>
+      <p>{props.userName ? props.userName : '...Username...'}</p>
+      <Link to='/signout'>Sign Out</Link>
+    </div>
+  </header>
 );
 
-const mapStateToProps = (state) => ({userName : state.main.userName});
+const mapStateToProps = (state) => ({ userName: state.main.userName });
 
 export default connect(mapStateToProps)(Header);
