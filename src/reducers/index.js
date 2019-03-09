b72e454d4dec17b6eee3b378f76ea89ac30becba
Shurap <bap.essp@mail.ru>
2019-03-09 13:12:32 +0300

    finished add exercise to store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,37 +2,32 @@ import { ADD_AUTH_USER_DATA } from '../constants';
 import { USER_OUT } from '../constants';
 import { ADD_NEW_EXERCISE } from '../constants'
 
-const defaultState = {currentUser:{
-  email: '',
-  exercises: [],
-  username: ''
-}};
+const defaultState = {
+  currentUser: {
+    email: '',
+    exercises: [],
+    username: ''
+  }
+};
 
 // export function mainReducer(state = {currentUser: {}}, action) {
 export function mainReducer(state = defaultState, action) {
   switch (action.type) {
     case ADD_AUTH_USER_DATA:
-      return {...state, currentUser: action.currentUser};
+      return { ...state, currentUser: action.currentUser };
     case USER_OUT:
-      return {...state, currentUser: action.currentUser};
+      return { ...state, currentUser: action.currentUser };
     case ADD_NEW_EXERCISE:
-      const dataToState = addToArrayState(action.label, state.currentUser.exercises);
-      console.log('4-', dataToState, state);
-      return {...state};
-
+      return {
+        ...state, currentUser: {
+          ...state.currentUser, exercises: state.currentUser.exercises.concat(action.label)
+        }
+      };
     default:
-      return state;  
+      return state;
   }
 }
 
-export function exercisesReducer(state = [1,2,3], action) {
+export function exercisesReducer(state = [1, 2, 3], action) {
   return state;
 }
-
-function addToArrayState(newData, where) {
-  const data = where;
-  console.log('0-', data);
-  data.push(newData);
-  console.log ('1-', where,'2-', newData,'3-', data);
-  return data;
- }
