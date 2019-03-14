5d99b0106de0b2efa7337a268223e7374e46c7f1
Shurap <bap.essp@mail.ru>
2019-03-14 23:13:50 +0300

    finished delete exercise

--- src/components/BlockListExercises/Exercise.js
+++ src/components/BlockListExercises/Exercise.js
@@ -1,19 +1,39 @@
 import React, {Component} from 'react';
 import styles from './Exercise.module.css';
+import { connect } from 'react-redux';
+import { withFirebase } from '../Firebase';
+import { addAuthUserData } from '../../actions';
+import { bindActionCreators } from 'redux';
 
-export default class Exercise extends Component {
+class Exercise extends Component {
 
   state = {
-    active: false
+    active: false,
+    error: null
   }
 
   onToggleSelected = () => {
     this.setState({...this.state, active: !this.state.active});
   }
 
+  onDelete = async (e) => {
+    const index = this.props.currentUser.exercises.findIndex(element => element === this.props.label);
+    const data = [...this.props.currentUser.exercises.slice(0,index), ...this.props.currentUser.exercises.slice(index+1)];
+    try {
+      e.preventDefault();
+      await this.props.firebase.setExercisesData(this.props.firebase.auth.currentUser.uid, data);
+      const user = await this.props.firebase.getUserById(this.props.firebase.auth.currentUser.uid);
+      this.props.addAuthUserData(user);
+    }
+    catch (error) {
+      this.setState({error});
+    }
+  }
+
   render() {
 
     const {label} = this.props;
+    const {error} = this.state;
 
     return (
       <div 
@@ -21,7 +41,14 @@ export default class Exercise extends Component {
         onClick={this.onToggleSelected}>
         <p>Exercise</p>
         {label}  
+        <button onClick={this.onDelete}>Del</button>
+        {error && <p>{error.message}</p>}
       </div>
     );
   }  
 }
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({ addAuthUserData }, dispatch);
+const mapStateToProps = (state) => ({ currentUser: state.main.currentUser });
+
+export default withFirebase(connect(mapStateToProps, mapDispatchToProps)(Exercise));
