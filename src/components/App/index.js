aa8c454a53c0c1f5b0f5d9d53db17f4fea42da88
Shurap <bap.essp@mail.ru>
2019-08-29 15:34:16 +0300

    fix small bugs

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Router from '../Router';
 import Header from '../Header';
+import Footer from '../Footer';
 import styles from './indexApp.module.css';
 import InfoWindow from '../InfoWindow';
 
 const App = () => (
-    <div className={styles.app}>
-      <Header/>
-      <InfoWindow/>
-      <Router/>
-    </div>
+  <div className={styles.app}>
+    <Header />
+    <InfoWindow />
+    <Router />
+    <Footer />
+  </div>
 );
 
 export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+const Footer = (props) => (
+  <footer className={styles.footer}>
+  </footer>
+);
+
+export default Footer;
